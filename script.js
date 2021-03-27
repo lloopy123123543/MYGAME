7cb3ebffc013f9202dc76d406d960855a0cab68b
Парфёнов Игорь Николаевич <pl8422@C201N6.pl1.edu>
2021-03-27 11:58:03 +0500

    loll

---
+++ script.js
@@ -0,0 +1,54 @@
+document.onkeydown = checkButton;
+
+let cube = {
+    x: 0,
+    y:0,
+    width: 56,
+    height: 56,
+    step: 50,
+    transition1:.25
+}
+
+function checkButton(event) {
+    if (event.keyCode === 37) {
+        console.log("ArrowLeft");
+        cube.x -= cube.step;
+        renderCube(cube);
+    }
+
+    else if (event.keyCode === 39) {
+        console.log("ArrowRight");
+        cube.x += cube.step;
+        renderCube(cube);
+    }
+
+    else if (event.keyCode === 40) {
+        console.log("ArrowDown");
+        cube.y += cube.step;
+        renderCube(cube);
+    }
+
+    else if (event.keyCode === 38) {
+        console.log('ArrowUp');
+        cube.y -= cube.step;
+        renderCube(cube);
+    }
+}
+
+function renderCube(cube) {
+    document.getElementById('cube').style.top = cube.y + 'px';
+    document.getElementById('cube').style.left = cube.x + 'px';
+    document.getElementById('cube').style.width = cube.width + 'px';
+    document.getElementById('cube').style.height = cube.height + 'px';
+    document.getElementById('cube').style.transition = 'ease' + cube.transition1 + 's';
+}
+
+renderCube(cube);
+//key: 'ArrowLeft'
+//keyCode: 37
+//key: 'ArrowRight'
+//keyCode: 39
+//key: 'ArrowDown'
+//keyCode: 40
+//key: 'ArrowUp'
+//keyCode: 38
